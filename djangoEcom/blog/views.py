56800d593997adfe1b3678fa0f1f20bb3c81adcb
Prakash Pandey <prksh.pandye@gmail.com>
2015-11-05 13:41:18 +0530

    pyc files deleted

--- djangoEcom/blog/views.py
+++ djangoEcom/blog/views.py
@@ -11,14 +11,14 @@ def blog(request):
     post_list = Post.objects.all()
     paginator = Paginator(post_list, 2)
     page = request.GET.get('page')
-    
+
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    
+
     context = {'posts':posts}
     return render(request, 'blog/blog.html', context)
 
